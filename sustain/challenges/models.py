c5b43fe335b976169f1a01e481a8e3995e8b3690
Mcginnis8 <camcginnis19@gmail.com>
2023-10-14 16:12:42 -0400

    j

--- sustain/challenges/models.py
+++ sustain/challenges/models.py
@@ -7,8 +7,8 @@ class Challenge(models.Model):
     description = models.CharField(max_length=1000)
     points = models.IntegerField(default=0)
     time = models.DateField(auto_now_add=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
     completionStatus = models.BooleanField(default=False)
     sustainer = models.ForeignKey(
         to=Sustainer,
--- sustain/challenges/views.py
+++ sustain/challenges/views.py
@@ -11,61 +11,64 @@ class ChallengeView(TemplateView):
         return render(request, self.template_name, context)
     
     def post(self, request):
-        support_cause = request.POST.get('support-dropdown')
-        hours_to_help = request.POST.get('hours-dropdown')
-        # Do something with the values
-        import openai
-        OPEN_AI_VAR = "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"
-        openai.api_key = OPEN_AI_VAR
+        if request.POST.get('location') == None or request.POST.get('location') == "Enter your location":
+            support_cause = request.POST.get('support-dropdown')
+            hours_to_help = request.POST.get('hours-dropdown')
+            # Do something with the values
+            import openai
+            OPEN_AI_VAR = "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"
+            openai.api_key = OPEN_AI_VAR
 
-        alignmentPrompt = """
-        You are a 1-hour challenge generator for an app called 'sustain' to create a more happy and sustainable world.
-        The challenges should be short and easy to complete.
-        They should not include posting on social media, as the app requires you to post a picture of your completed challenge.
-        You only give 1-2 sentence challenges in the following format:
-        Hi User,
-        Your challenge is: {insert_challenge_here}
-        Go Forth and sustain!
-        """
+            alignmentPrompt = """
+            You are a 1-hour challenge generator for an app called 'sustain' to create a more happy and sustainable world.
+            The challenges should be short and easy to complete.
+            They should not include posting on social media, as the app requires you to post a picture of your completed challenge.
+            You only give 1-2 sentence challenges in the following format:
+            Hi User,
+            Your challenge is: {insert_challenge_here}
+            Go Forth and sustain!
+            """
 
-        time_limit = hours_to_help
-        cause = support_cause
-        UserPrompt = f"Create a challenge to help the following cause:{cause} with a time limit of ${time_limit}"
+            time_limit = hours_to_help
+            cause = support_cause
+            UserPrompt = f"Create a challenge to help the following cause:{cause} with a time limit of ${time_limit}"
 
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": alignmentPrompt},
-                {"role": "user", "content": UserPrompt}
-            ]
-        )
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": alignmentPrompt},
+                    {"role": "user", "content": UserPrompt}
+                ]
+            )
 
-        rawChallenge = response['choices'][0]['message']['content']
-        try:
-            parsedChallenge = rawChallenge.splitlines()[1].strip().split("is: ")[1]
-        except:
-            parsedChallenge = rawChallenge
-        print(rawChallenge)
-        print(parsedChallenge)
-        print(response['usage'])
+            rawChallenge = response['choices'][0]['message']['content']
+            try:
+                parsedChallenge = rawChallenge.splitlines()[1].strip().split("is: ")[1]
+            except:
+                parsedChallenge = rawChallenge
+            print(rawChallenge)
+            print(parsedChallenge)
+            print(response['usage'])
+            
+            new = Challenge(cause=cause, description=parsedChallenge, points=time_limit, sustainer=request.user.sustainer)
+            new.save()
+            context = {
+                'challenges': Challenge.objects.filter(sustainer=request.user.sustainer),
+            }
+            
+
+            return render(request, self.template_name, context)
+        else:
+            challenge_location = request.POST.get('location')
+            challenge_picture = request.POST.get('profile-picture')
+            curr_pk = request.POST.get('challenge_pk')
+            challenge = Challenge.objects.get(id=curr_pk)
+            challenge.latitude = float(challenge_location.split(",")[0].strip())
+            challenge.longitude = float(challenge_location.split(",")[1].strip())
+            challenge.completionStatus = True
+            challenge.save()
+            return redirect('challenges:challenges')
         
-        new = Challenge(cause=cause, description=parsedChallenge, points=time_limit, sustainer=request.user.sustainer)
-        new.save()
-        context = {
-            'challenges': Challenge.objects.filter(sustainer=request.user.sustainer),
-        }
-        
-
-        return render(request, self.template_name, context)
-    
-    
-class ChallengeCompleteView(TemplateView):
-    
-    def get(self, request, pk):
-        challenge = Challenge.objects.get(id=pk)
-        challenge.completionStatus = True
-        challenge.save()
-        return redirect('challenges:challenges')
     
     
 class ChallengeDeleteView(TemplateView):
