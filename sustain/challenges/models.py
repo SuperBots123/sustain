a24b9d667c8a9f65bd5754534005bed1b5151158
tawfiqfm <tawfiqfm@gmail.com>
2023-10-14 14:09:23 -0400

    Merging map circle to new feed page

--- sustain/challenges/models.py
+++ sustain/challenges/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import Sustainer
 
-# Create your models here.
+class Challenge(models.Model):
+    sustainer = models.ForeignKey(Sustainer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # add other fields as needed
--- sustain/profiles/views.py
+++ sustain/profiles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from challenges.models import Challenge
+
 
 # Create your views here.
+class ProfileView(TemplateView):
+    template_name = 'profiles/profile.html'
+    def get(self, request):
+            # self.sustainer.user
+        sustainer_challenges = Challenge.objects.filter(sustainer=request.user.sustainer)
+        context = {
+            'sustainer_challenges':sustainer_challenges
+        }
+        return render(request, self.template_name, context)
+        
+    
