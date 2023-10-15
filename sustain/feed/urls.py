79a876368cd89604d7acd30558e7017416952186
tawfiqfm <tawfiqfm@gmail.com>
2023-10-15 00:01:44 -0400

    friend profile pictures

--- sustain/feed/urls.py
+++ sustain/feed/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'feed'
@@ -7,3 +8,5 @@ urlpatterns = [
     path('', views.FeedView.as_view(), name='feed'),
     path('follow/<int:pk>', views.FollowView.as_view(), name='follow')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
