6e4fdb9cf701f9889f9435873dc90a20e3df5de5
Garrett Meek <bmeek6@gatech.edu>
2023-10-14 22:04:47 -0400

    Friend suggestions

--- sustain/feed/urls.py
+++ sustain/feed/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'feed'
 urlpatterns = [
     path('', views.FeedView.as_view(), name='feed'),
+    path('follow/<int:pk>', views.FollowView.as_view(), name='follow')
 ]
